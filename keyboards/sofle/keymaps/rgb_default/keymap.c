71306a30987e021852a71896ec8c0b76ac5c6db5
Nicolas Salas <nicolasgsalas@gmail.com>
2023-11-03 09:05:55 -0700

    general remaping

--- keyboards/sofle/keymaps/rgb_default/keymap.c
+++ keyboards/sofle/keymaps/rgb_default/keymap.c
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
 	KC_ESC,	 		KC_1,	KC_2,		KC_3,		KC_4,			KC_5,												KC_6,		KC_7,			KC_8,		KC_9,	KC_0,		KC_BSLS,
 	MO(KC_CAPS),	KC_Q,	KC_W,		KC_E,		KC_R,			KC_T,												KC_Y,		KC_U,			KC_I,		KC_O,	KC_P,		KC_MINS,
-	SH_T(KC_SHRUG),	    KC_A,	KC_S,		KC_D,		KC_F,			KC_G,												KC_H,		KC_J,			KC_K,		KC_L,	LT(_NAV, KC_SCLN),	SH_T(KC_QUOT),
+	SH_T(KC_SHRUG),	    LT(_NAV, KC_A),	KC_S,		KC_D,		KC_F,			KC_G,												KC_H,		KC_J,			KC_K,		KC_L,	LT(_NAV, KC_SCLN),	SH_T(KC_QUOT),
 	KC_GRV,	 		KC_Z,	KC_X,		KC_C,		KC_V,			KC_B, 		XXXXXXX,			XXXXXXX, 			KC_N,		KC_M,			KC_COMM,	KC_DOT,	KC_SLSH,	KC_EQL,
 							KC_LGUI,	TO(_GAME),  ALT_T(XXXXXXX),	CTL_T(KC_TAB),	SFT_T(KC_ENT),	SFT_T(KC_SPC),		CTL_T(KC_BSPC),	ALT_T(KC_DEL),		TO(_GAME),	KC_LBRC
 ),
@@ -353,7 +353,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case KC_SHRUG:
 			if (record->event.pressed) {
-				SEND_STRING("¯\\_(ツ)_/¯");
+				SEND_STRING("Hello world!");
+                // SEND_STRING("¯\\_(ツ)_/¯");
 			}
             return false;
 	}
