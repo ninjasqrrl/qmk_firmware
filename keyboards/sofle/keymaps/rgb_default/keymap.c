4883ac2c0432ed63c788da7f4f2f892b1c16091d
Nicolas Salas <nicolasgsalas@gmail.com>
2023-11-09 11:55:24 -0800

    swapped the bracket characters

--- keyboards/sofle/keymaps/rgb_default/keymap.c
+++ keyboards/sofle/keymaps/rgb_default/keymap.c
@@ -104,8 +104,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT(
 	KC_ESC,	 		KC_1,			KC_2,		KC_3,		KC_4,			KC_5,											KC_6,			KC_7,			KC_8,		KC_9,	KC_0,				KC_EQL,
-	KC_LBRC,		KC_Q,			KC_W,		KC_E,		KC_R,			KC_T,											KC_Y,			KC_U,			KC_I,		KC_O,	KC_P,				KC_MINS,
-	SH_T(KC_RBRC),	LT(_NAV, KC_A),	KC_S,		KC_D,		KC_F,			KC_G,											KC_H,			KC_J,			KC_K,		KC_L,	LT(_NAV, KC_SCLN),	SH_T(KC_QUOT),
+	KC_RBRC,		KC_Q,			KC_W,		KC_E,		KC_R,			KC_T,											KC_Y,			KC_U,			KC_I,		KC_O,	KC_P,				KC_MINS,
+	SH_T(KC_LBRC),	LT(_NAV, KC_A),	KC_S,		KC_D,		KC_F,			KC_G,											KC_H,			KC_J,			KC_K,		KC_L,	LT(_NAV, KC_SCLN),	SH_T(KC_QUOT),
 	KC_GRV,	 		KC_Z,			KC_X,		KC_C,		KC_V,			KC_B, 			XXXXXXX,		XXXXXXX, 		KC_N,			KC_M,			KC_COMM,	KC_DOT,	KC_SLSH,			KC_BSLS,
 									XXXXXXX,	TO(_GAME),	ALT_T(KC_LGUI),	CTL_T(KC_TAB),	SFT_T(KC_ENT),	SFT_T(KC_SPC),	CTL_T(KC_BSPC),	ALT_T(KC_DEL),	TO(_GAME),	XXXXXXX
 ),
